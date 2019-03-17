36417d473cb917ed222fe8c4354109545eef3622
Hugo J. Bello <hjbello.wk@gmail.com>
2019-03-17 18:06:28 +0100

    fix

--- src/components/MapResultsOpenlayers.jsx
+++ src/components/MapResultsOpenlayers.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import 'ol/ol.css'
-import {Map, View} from 'ol'
-import TileLayer from 'ol/layer/Tile'
-import {fromLonLat} from 'ol/proj'
-import XYZ from 'ol/source/XYZ'
-import {defaults, ScaleLine} from 'ol/control'
-import GeoJSON from 'ol/format/GeoJSON';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import Circle from 'ol/geom/Circle.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
+import {fromLonLat} from "ol/proj";
+
 
 export class MapResultsOpenlayers extends Component {
     constructor(props) {
@@ -16,47 +18,189 @@ export class MapResultsOpenlayers extends Component {
     };
 
     getLayers = () => {
-        const geojson = this.props.geojson;
-        console.log(geojson);
-        const crsStr = this.state.crs;
-        geojson.crs = {
-            'type': 'name',
-                'properties': {
-                'name': crsStr
-            }
+
+
+        var image = new CircleStyle({
+            radius: 5,
+            fill: null,
+            stroke: new Stroke({color: 'red', width: 1})
+        });
+
+        var styles = {
+            'Point': new Style({
+                image: image
+            }),
+            'LineString': new Style({
+                stroke: new Stroke({
+                    color: 'green',
+                    width: 1
+                })
+            }),
+            'MultiLineString': new Style({
+                stroke: new Stroke({
+                    color: 'green',
+                    width: 1
+                })
+            }),
+            'MultiPoint': new Style({
+                image: image
+            }),
+            'MultiPolygon': new Style({
+                stroke: new Stroke({
+                    color: 'yellow',
+                    width: 1
+                }),
+                fill: new Fill({
+                    color: 'rgba(255, 255, 0, 0.1)'
+                })
+            }),
+            'Polygon': new Style({
+                stroke: new Stroke({
+                    color: 'blue',
+                    lineDash: [4],
+                    width: 3
+                }),
+                fill: new Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+            }),
+            'GeometryCollection': new Style({
+                stroke: new Stroke({
+                    color: 'magenta',
+                    width: 2
+                }),
+                fill: new Fill({
+                    color: 'magenta'
+                }),
+                image: new CircleStyle({
+                    radius: 10,
+                    fill: null,
+                    stroke: new Stroke({
+                        color: 'magenta'
+                    })
+                })
+            }),
+            'Circle': new Style({
+                stroke: new Stroke({
+                    color: 'red',
+                    width: 2
+                }),
+                fill: new Fill({
+                    color: 'rgba(255,0,0,0.2)'
+                })
+            })
         };
 
-        const vectorSource = new VectorSource({
-            renderMode: 'image',
-            format: new GeoJSON(),
-            projection: crsStr,
+        var styleFunction = function(feature) {
+            return styles[feature.getGeometry().getType()];
+        };
+
+        var geojsonObject = {
+            'type': 'FeatureCollection',
+            'crs': {
+                'type': 'name',
+                'properties': {
+                    'name': 'EPSG:3857'
+                }
+            },
+            'features': [{
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [0, 0]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [[4e6, -2e6], [8e6, 2e6]]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [[4e6, 2e6], [8e6, -2e6]]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'MultiLineString',
+                    'coordinates': [
+                        [[-1e6, -7.5e5], [-1e6, 7.5e5]],
+                        [[1e6, -7.5e5], [1e6, 7.5e5]],
+                        [[-7.5e5, -1e6], [7.5e5, -1e6]],
+                        [[-7.5e5, 1e6], [7.5e5, 1e6]]
+                    ]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'MultiPolygon',
+                    'coordinates': [
+                        [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
+                        [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
+                        [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]]
+                    ]
+                }
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'GeometryCollection',
+                    'geometries': [{
+                        'type': 'LineString',
+                        'coordinates': [[-5e6, -5e6], [0, -5e6]]
+                    }, {
+                        'type': 'Point',
+                        'coordinates': [4e6, -5e6]
+                    }, {
+                        'type': 'Polygon',
+                        'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]]
+                    }]
+                }
+            }]
+        };
+
+        //const projection = "EPSG:4326";
+        const projection = "EPSG:900913";
+        const geojson = this.props.geojson;
+
+        geojson.crs = {
+            'type': 'name',
+            'properties': {
+                'name': projection
+            }
+        };
+        var vectorSource = new VectorSource({
             features: (new GeoJSON()).readFeatures(geojson)
         });
 
-        const vectorLayer = new VectorLayer({
-            source: vectorSource
+        //vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
+
+        var vectorLayer = new VectorLayer({
+            source: vectorSource,
+            style: styleFunction
         });
 
-        const tileLayer = new TileLayer({
-            title: "annotation",
-            source: new XYZ({
-                url: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-        });
-
-
-        let map = new Map({
-            target: 'basemap',
+        var map = new Map({
             layers: [
-                vectorLayer,
-                tileLayer
+                new TileLayer({
+                    source: new OSM()
+                }),
+                vectorLayer
             ],
+            target: 'map',
             view: new View({
-                projection: crsStr,
+                projection: projection,
                 center: fromLonLat([-3, 40]),
                 zoom: 8
             })
-        })
+        });
 
     }
 
@@ -68,7 +212,7 @@ export class MapResultsOpenlayers extends Component {
 
     render() {
         return (
-            <div id="basemap" className='basemap'></div>
+            <div id="map" className='basemap'></div>
         )
     }
 }
